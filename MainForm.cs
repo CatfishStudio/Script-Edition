17e78a3020d91d02ef32a1e5e5944a057597aecd
CatfishStudio <somov@i.ua>
2014-04-03 09:22:41 +0300

    Create project Script Edition: 03-04-2014  9:22

---
+++ MainForm.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Сделано в SharpDevelop.
+ * Пользователь: Catfish
+ * Дата: 01.04.2014
+ * Время: 9:18
+ * 
+ * Для изменения этого шаблона используйте Сервис | Настройка | Кодирование | Правка стандартных заголовков.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ScriptEdition
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Сделано в SharpDevelop.
+ * Пользователь: Catfish
+ * Дата: 01.04.2014
+ * Время: 9:18
+ * 
+ * Для изменения этого шаблона используйте Сервис | Настройка | Кодирование | Правка стандартных заголовков.
+ */
+using System;
+using System.Windows.Forms;
+
+namespace ScriptEdition
+{
+	/// <summary>
+	/// Class with program entry point.
+	/// </summary>
+	internal sealed class Program
+	{
+		/// <summary>
+		/// Program entry point.
+		/// </summary>
+		[STAThread]
+		private static void Main(string[] args)
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new MainForm());
+		}
+		
+	}
+}
